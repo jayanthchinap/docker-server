0c4b06b0739b1a2401309778acd630a04b2dd1cc
jaayanthchinap <jayanthbrahmanapelly9@gmail.com>
2022-07-07 07:13:46 +0000

    last commit

--- new-fastapi-master/login-main/src/components/ViewItem/index.js
+++ new-fastapi-master/login-main/src/components/ViewItem/index.js
@@ -12,7 +12,7 @@ class ViewItem extends Component {
     data: [],
   };
   getData = () => {
-    axios.get("http://159.65.151.35/users/").then((response) => {
+    axios.get("http://159.65.151.35:5000/users/").then((response) => {
       this.setState({ data: response?.data });
     });
   };
@@ -27,7 +27,7 @@ class ViewItem extends Component {
           label: "Yes",
           onClick: () =>
             axios
-              .delete(`http://159.65.151.35/users/${id}`)
+              .delete(`http://159.65.151.35:5000/users/${id}`)
               .then((response) => {
                 this.getData();
               }),
